62ce37c983dfb57adc5140a952ff66e471517b46
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-04-30 23:17:17 +0900

    init spring

---
+++ src/main/java/com/hand/dot/LabelmakerApplication.java
@@ -0,0 +1,12 @@
+package com.hand.dot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabelmakerApplication.class, args);
+	}
+}
