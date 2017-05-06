a9e2884f889fdb09b67b2839ccf3f2be45bc511a
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-05-06 23:03:42 +0900

    change pom for tomcat deploy

--- src/main/java/com/hand/dot/LabelmakerApplication.java
+++ src/main/java/com/hand/dot/LabelmakerApplication.java
@@ -2,11 +2,16 @@ package com.hand.dot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LabelmakerApplication {
-
+public class LabelmakerApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(LabelmakerApplication.class, args);
 	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LabelmakerApplication.class);
+    }
 }
