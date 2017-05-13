d538603140c2026292f69d41bac2e43bf3b23128
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-05-13 15:13:42 +0900

    add webpack plugin

--- src/main/resources/static/webpack.config.js
+++ src/main/resources/static/webpack.config.js
@@ -1,11 +1,11 @@
 var path = require('path')
 var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
         filename: 'build.js'
     },
     module: {
@@ -58,7 +58,14 @@ module.exports = {
     performance: {
         hints: false
     },
-    devtool: '#eval-source-map'
+    devtool: '#eval-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ]
 }
 
 if (process.env.NODE_ENV === 'production') {
